2fce9bdf5d0121d7b64fff66a91c0f00263cddd0
Nikita Gubenko <nikita.gubenko@gmail.com>
2014-04-10 17:47:46 +0400

    removed hardcoded tenant_id

--- report.py
+++ report.py
@@ -6,12 +6,12 @@ import json
 
 
 def report():
-    token = authorize()
-    active_vms = get_all_active_vms(token)
+    auth_data = authorize()
+    active_vms = get_all_active_vms(auth_data)
     active_projects = get_active_projects(active_vms)
     print('Total number of active projects is: %s') % len(active_projects)
     print('Total number of vms is: %s') % len(active_vms.keys())
-    resources = get_resources_usage(token)
+    resources = get_resources_usage(auth_data)
     print('Total number of computes: %s') % resources['count']
     print('VCPUs used/total: %s/%s') % (resources['vcpus_used'], resources['vcpus'])
     print('RAM (Mb) used/total: %s/%s') % (resources['memory_mb_used'], resources['memory_mb'])
@@ -19,6 +19,7 @@ def report():
 
 
 def authorize():
+    auth_data = {}
     # add check of variables
     # need to replace v2 with v3 if possible here
     url = os.environ['KEYSTONE_ENDPOINT'] + '/tokens'
@@ -31,7 +32,10 @@ def authorize():
             }
         }
     }
-    return api_request(url, params, None)['access']['token']['id']
+    responce = api_request(url, params, None)
+    auth_data['token'] = responce['access']['token']['id']
+    auth_data['tenant_id'] = responce['access']['token']['tenant']['id']	
+    return auth_data
 
 
 def api_request(url, params, headers):
@@ -47,11 +51,10 @@ def api_request(url, params, headers):
     return json.load(response)
 
 
-def get_all_active_vms(token):
-    # remove hardcode
+def get_all_active_vms(auth_data):
     active_vms = {}
-    url = os.environ['COMPUTE_ENDPOINT'] + '/1445d1b184b1402982db31d4b4756796/servers/detail?all_tenants=1'
-    for server in api_request(url, None, {"X-Auth-Token": token})['servers']:
+    url = os.environ['COMPUTE_ENDPOINT'] + '/' + auth_data['tenant_id'] + '/servers/detail?all_tenants=1'
+    for server in api_request(url, None, {"X-Auth-Token": auth_data['token']})['servers']:
         active_vms[server['id']] = {'name': server['name'], 'project_id': server['tenant_id']}
     return active_vms
 
@@ -65,10 +68,9 @@ def get_active_projects(active_vms):
     return active_projects
 
 
-def get_resources_usage(token):
-    # remove hardcode
-    url = os.environ['COMPUTE_ENDPOINT'] + '/1445d1b184b1402982db31d4b4756796/os-hypervisors/statistics'
-    return api_request(url, None, {"X-Auth-Token": token})['hypervisor_statistics']
+def get_resources_usage(auth_data):
+    url = os.environ['COMPUTE_ENDPOINT'] + '/' + auth_data['tenant_id'] + '/os-hypervisors/statistics'
+    return api_request(url, None, {"X-Auth-Token": auth_data['token']})['hypervisor_statistics']
 
 
 if __name__ == '__main__':
